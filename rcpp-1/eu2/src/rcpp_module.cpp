935202abac5ec3fad1880cfb418a3c283a841a5e
Eugene Skepner <github@skepner.eu>
2017-12-14 15:26:43 +0100

    ss

--- rcpp-1/eu2/src/rcpp_module.cpp
+++ rcpp-1/eu2/src/rcpp_module.cpp
@@ -20,38 +20,46 @@
 // along with Rcpp.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <iostream>
+
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wdelete-non-virtual-dtor"
+#endif
+
 #include <Rcpp.h>
 
-std::string hello() {
-    throw std::range_error( "boom" ) ;
-}
 
-int bar(int x) {
-    return x*2;
-}
+// std::string hello() {
+//     throw std::range_error( "boom" ) ;
+// }
 
-double foo(int x, double y) {
-    return x * y;
-}
+// int bar(int x) {
+//     return x*2;
+// }
 
-void bla() {
-    Rprintf("hello\\n");
-}
+// double foo(int x, double y) {
+//     return x * y;
+// }
 
-void bla1(int x) {
-    Rprintf("hello (x = %d)\\n", x);
-}
+// void bla() {
+//     Rprintf("hello\\n");
+// }
 
-void bla2( int x, double y) {
-    Rprintf("hello (x = %d, y = %5.2f)\\n", x, y);
-}
+// void bla1(int x) {
+//     Rprintf("hello (x = %d)\\n", x);
+// }
+
+// void bla2( int x, double y) {
+//     Rprintf("hello (x = %d, y = %5.2f)\\n", x, y);
+// }
 
 class World {
 public:
-    World() : msg("hello") { Rprintf("constructor\n"); }
-    ~World() { Rprintf("destructor %s\n", msg.data()); }
+    World() : msg("hello") { Rcpp::Rcout << "constructor " << msg << '\n'; }
+    ~World() { Rcpp::Rcout << "destructor " << msg << '\n'; }
     void set(std::string msg) { this->msg = msg; }
-    std::string greet() { return msg; }
+    std::string greet() const { return msg; }
+
+      // static void fin(World* w) { Rcpp::Rcout << "fin\n"; }
 
 private:
     std::string msg;
@@ -62,24 +70,26 @@ private:
 RCPP_MODULE(yada){
     using namespace Rcpp ;
 
-    function("hello" , &hello  , "documentation for hello ");
-    function("bla"   , &bla    , "documentation for bla ");
-    function("bla1"  , &bla1   , "documentation for bla1 ");
-    function("bla2"  , &bla2   , "documentation for bla2 ");
+    // function("hello" , &hello  , "documentation for hello ");
+    // function("bla"   , &bla    , "documentation for bla ");
+    // function("bla1"  , &bla1   , "documentation for bla1 ");
+    // function("bla2"  , &bla2   , "documentation for bla2 ");
 
-    // with formal arguments specification
-    function("bar"   , &bar    ,
-             List::create( _["x"] = 0.0),
-             "documentation for bar ");
-    function("foo"   , &foo    ,
-             List::create( _["x"] = 1, _["y"] = 1.0),
-             "documentation for foo ");
+    // // with formal arguments specification
+    // function("bar"   , &bar    ,
+    //          List::create( _["x"] = 0.0),
+    //          "documentation for bar ");
+    // function("foo"   , &foo    ,
+    //          List::create( _["x"] = 1, _["y"] = 1.0),
+    //          "documentation for foo ");
 
-    class_<World>("World")
+    class_<World>("my.world")
     // expose the default constructor
-    .constructor()
+            .constructor()
+              // .finalizer(&World::fin)
+            .property("p", static_cast<std::string (*)(World*)>([](World* w) { return w->greet(); }), static_cast<void (*)(World*, std::string)>([](World* w, std::string new_msg) { w->set(new_msg); }), "greet/set")
+            .method("greet", &World::greet , "get the message")
+            .method("set", &World::set     , "set the message")
 
-    .method("greet", &World::greet , "get the message")
-    .method("set", &World::set     , "set the message")
     ;
 }
