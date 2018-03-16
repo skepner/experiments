df6b0ab863bfaf3152efeb38ce4bc826691cf39c
Eugene Skepner <github@skepner.eu>
2018-03-16 17:12:28 +0100

    ss

--- marina.skepner.eu/main.js
+++ marina.skepner.eu/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     create_sidebar();
     $(window).resize(window_resized);
     window_resized();
-    $("iframe").load(set_iframe);
+    // $("iframe").load(set_iframe);
 });
 
 function fix_menu()
@@ -24,12 +24,45 @@ function create_sidebar()
     $('.ui.sidebar').sidebar('attach events', '.toc.item');
 }
 
+// ----------------------------------------------------------------------
+
 var sLabels = {
     email_address: "zuskepner@gmail.com",
     email: "Maile mir",
     phone: "+49 152 33605790",
     call: "Ruf mich an"
 };
+
+// ----------------------------------------------------------------------
+
+var sPages = {
+    "portrait-drawing": {
+        divs: [
+            {img: "chloe-ricoh", cap: "portraets-auf-bestellung"},
+            {img: "chloe-ricoh", cap: "portraits"}
+        ]
+    },
+    "collage": {
+    },
+    "you-are-here": {
+    },
+    "cococolaboration": {
+    },
+    "das-bonner-buerger-buch": {
+    },
+    "ten-letters-to-artist": {
+    },
+    "my-endless-book": {
+    },
+    "a-chloe-a-day": {
+    },
+    "paintings-2012": {
+    }
+};
+
+var sPage = "portrait-drawing";
+
+// ----------------------------------------------------------------------
 
 var sDevTargets = {
     vmob: {
@@ -70,8 +103,13 @@ function window_resized()
     set_target();
 }
 
-
 function set_target()
+{
+    set_top_menu();
+    set_page();
+}
+
+function set_top_menu()
 {
     $('body').removeClass(Object.keys(sDevTargets).join(" "));
     $('body').addClass(sDevTarget);
@@ -79,16 +117,23 @@ function set_target()
     $('#email').text(target.email);
     $('#phone').text(target.phone);
 
-    set_iframe();
-
     // $('.top.main.fixed .item').addClass("tiny");
-
     // $("#top-bar").addClass("tiny");
     // $("#top-bar .item").addClass("tiny");
 }
 
-function set_iframe()
+function set_page()
 {
-    var iframe = $("iframe").contents();
-    iframe.find('img.picture').width($(window).width());
+    $("#page-top").height($("#top-menu").height());
+    $("#page").empty();
+    $("#whole-page caption").hide();
+
+    var page_data = sPages[sPage];
+    for (var element of page_data.divs) {
+        var img = $("<img src='" + element.img + "-" + sDevTarget[0] + "-1.78-414.jpg' width='" + $(window).width() + "' height='" + ($(window).height() - $("#top-menu").height()) + "'>").appendTo("#page");
+        var img_pos = img.offset(), img_height = img.height();
+        // console.log("img pos " + JSON.stringify(img_pos));
+        var cap = $("#" + sPage + "_" + element.cap).css({top: img_pos.top + img_height, left: img_pos.left}).show();
+        cap.css("top", cap.position().top - cap.height());
+    }
 }
