486eeee8feaca7456b3f3b5e0f33f281c15026bd
Eugene Skepner <github@skepner.eu>
2018-05-07 14:03:22 +0200

    ss

--- apache-mod/mod_ace.cc
+++ apache-mod/mod_ace.cc
@@ -5,27 +5,32 @@
 #include "apache2/http_protocol.h"
 #include "apache2/http_request.h"
 
+#include "acmacs-base/read-file.hh"
+
 // #include "apr_hooks.h"
 
 static void register_hooks(apr_pool_t *pool);
-static int example_handler(request_rec *r);
+static int ace_handler(request_rec *r);
 
-extern "C" module example_module;
+extern "C" module ace_module;
 
-module AP_MODULE_DECLARE_DATA example_module = {
+module AP_MODULE_DECLARE_DATA ace_module = {
     STANDARD20_MODULE_STUFF, NULL, NULL, NULL, NULL, NULL, register_hooks
 };
 
 static void register_hooks(apr_pool_t *pool) {
-    ap_hook_handler(example_handler, NULL, NULL, APR_HOOK_LAST);
+    ap_hook_handler(ace_handler, NULL, NULL, APR_HOOK_LAST);
 }
 
-static int example_handler(request_rec *r) {
-    if (!r->handler || strcmp(r->handler, "example"))
+static int ace_handler(request_rec *r) {
+    if (!r->handler || r->handler != std::string("ace"))
         return (DECLINED);
 
+    const std::string data = acmacs::file::read(r->filename);
+
     ap_set_content_type(r, "application/json");
-    ap_rprintf(r, "{N: \"Hello, world! filename:[%s] args:[%s]\", b:2, c:3, d:{a:2}}\n\n", r->filename, r->args);
+      // ap_rprintf(r, "{N: \"Hello, world! filename:[%s] args:[%s]\", b:2, c:3, d:{a:2}}\n\n", r->filename, r->args);
+    ap_rputs(data.c_str(), r);
     return OK;
 }
 
---
+++ apache-mod/mod_ace.cpp
@@ -0,0 +1,1 @@
+mod_ace.cc
