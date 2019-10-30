5604f958a70866c75aaa19bfef2464aa13473696
Eugene Skepner <github@skepner.eu>
2019-10-30 15:51:27 +0100

    ss

--- gm/r1.py
+++ gm/r1.py
@@ -36,10 +36,10 @@ import pprint
 
 # Creating a storage.JSON file with authentication details
 SCOPES = 'https://www.googleapis.com/auth/gmail.modify' # we are using modify and not readonly, as we will be marking the messages Read
-store = file.Storage('storage.json')
+store = file.Storage('/d/gm/storage.json')
 creds = store.get()
 if not creds or creds.invalid:
-    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    flow = client.flow_from_clientsecrets('/d/gm/client_secret.json', SCOPES)
     creds = tools.run_flow(flow, store)
 GMAIL = discovery.build('gmail', 'v1', http=creds.authorize(Http()))
 
